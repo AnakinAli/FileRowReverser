985126fd5a613da0a43fd25c5e446660041d91ad
AnakinAli <anakin48@abv.bg>
2019-08-24 19:47:50 +0200

    Check DataWithPrepend out

---
+++ FileRowReverser/DataWithPrepend/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DataWithPrepend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1000000 Sales Records.csv
+            string lines = String.Join("\n", File
+                .ReadAllText(@"..\1000000 Sales Records.csv")
+                .Split(new char[] { '\n'})
+                .Select(l=>String.Join(" ",l.Split(',').Reverse()))
+                .Reverse());
+
+            File.WriteAllText(@"..\Output.txt", lines);
+        }
+    }
+}
